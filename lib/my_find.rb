a8e6853a51f9b20c6c2ac64464989829de2f1d9b
Pablo Menz <p.menz.r@gmail.com>
2020-03-13 17:59:50 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,12 +1,9 @@
 require 'pry'
 
 def my_find(collection)
-  i = 15
+  i = 0
   while i < collection.length
-    if yield(collection[i])
-    return collection[i]
+    return collection[i] if yield(collection[i])
     i = i + 1
   end
-end
-
 end
